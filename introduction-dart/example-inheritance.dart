17807b138f9a1d9add0944186503b74b441a25bc
Kieu Linh <hothikieulinh1407@gmail.com>
2023-11-15 17:18:23 +0700

    example about introduction dart

---
+++ introduction-dart/example-inheritance.dart
@@ -0,0 +1,51 @@
+// example inheritance in dart
+class Person {
+  String? name;
+  int? age;
+  String? address;
+  double? height;
+
+  Person({this.name, this.age, this.address, this.height});
+
+  String getName() => name ?? '';
+  int getAge() => age ?? 0;
+  String getAddress() => address ?? '';
+  double getHeight() => height ?? 0;
+  void setName(String name) => this.name = name;
+  void setAge(int age) => this.age = age;
+  void setAddress(String address) => this.address = address;
+  void setHeight(double height) => this.height = height;
+
+  @override
+  String toString() {
+    return 'Name: $name, Age: $age, Address: $address, Height: $height';
+  }
+}
+
+class Student extends Person {
+  String? school;
+  String? className;
+
+  Student({this.school, this.className});
+
+  String getSchool() => school ?? '';
+  String getClassName() => className ?? '';
+  void setSchool(String school) => this.school = school;
+  void setClassName(String className) => this.className = className;
+
+  @override
+  String toString() {
+    return 'Name: $name, Age: $age, Address: $address, Height: $height, School: $school, Class: $className';
+  }
+}
+
+void main() {
+  Student student = Student();
+  student.setName('Linh');
+  student.setAge(20);
+  student.setAddress('Da Nang');
+  student.setHeight(1.6);
+  student.setSchool('Su Pham');
+  student.setClassName('CNTT');
+  print(student.toString());
+}
