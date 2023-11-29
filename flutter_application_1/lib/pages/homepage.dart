13254e98a812c679ba4dc897b21a11bef92e2627
Kieu Linh <hothikieulinh1407@gmail.com>
2023-11-29 11:01:06 +0700

    example about Stack

--- flutter_application_1/lib/pages/homepage.dart
+++ flutter_application_1/lib/pages/homepage.dart
@@ -10,23 +10,46 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('This is a Row1'),
-            SizedBox(height: 20.0),
-            Text('This is a Row2'),
-            SizedBox(height: 20.0),
-            Text('This is a Row3'),
-            SizedBox(height: 20.0),
-          ],
-        )
-      ],
+    return Scaffold(
+        body: Center(
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Text(
+            'Example Stack',
+            style: TextStyle(
+              color: Colors.pink.withOpacity(0.5),
+              fontSize: 20.0,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          const Positioned(
+            top: 20,
+            child: Icon(
+              Icons.star,
+              size: 100,
+              color: Colors.yellow,
+            ),
+          )
+        ],
+      ),
     )
-    );
+        //  Column(
+        //   crossAxisAlignment: CrossAxisAlignment.center,
+        //   children: [
+        //     Row(
+        //       mainAxisAlignment: MainAxisAlignment.center,
+        //       children: [
+        //         Text('This is a Row1'),
+        //         SizedBox(height: 20.0),
+        //         Text('This is a Row2'),
+        //         SizedBox(height: 20.0),
+        //         Text('This is a Row3'),
+        //         SizedBox(height: 20.0),
+        //       ],
+        //     ),
+        //   ],
+        // ),
+        );
   }
 }
