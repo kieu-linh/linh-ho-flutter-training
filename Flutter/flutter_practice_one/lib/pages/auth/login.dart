3e7b375498add4001e4e71bbb50bd646b1ae76ce
Kieu Linh <hothikieulinh1407@gmail.com>
2023-12-14 11:23:44 +0700

    Done update login screen

--- Flutter/flutter_practice_one/lib/pages/auth/login.dart
+++ Flutter/flutter_practice_one/lib/pages/auth/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_practice_one/core/typography/font_weight.dart';
 import 'package:flutter_practice_one/core/typography/text_style.dart';
 import 'package:flutter_practice_one/widgets/app_bar_auth.dart';
 import 'package:flutter_practice_one/widgets/button.dart';
@@ -18,56 +19,82 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       appBar: const AppBarAuth(),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20).copyWith(top: 30),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Welcome to Pro Fitness!',
-                style: AppTextStyles.headlineLarge,
-              ),
-              const SizedBox(height: 11),
-              Text(
-                'Hello there, sign in to \ncontinue!',
-                style: AppTextStyles.headlineMedium,
-              ),
-              const SizedBox(height: 39),
-              const FATextField(
-                hintText: 'Email Address',
-                icon: 'assets/icons/ic_tick.svg',
-              ),
-              const SizedBox(height: 14),
-              const FATextFieldPassword(
-                hintText: 'Password',
-              ),
-              const SizedBox(height: 16),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Text(
-                    'Forgot Password?',
-                    style: AppTextStyles.textTitle,
-                    textAlign: TextAlign.right,
+        padding: const EdgeInsets.symmetric(horizontal: 20).copyWith(top: 35),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Welcome to Pro Fitness!',
+              style: AppTextStyles.headlineLarge,
+            ),
+            const SizedBox(height: 11),
+            Text(
+              'Hello there, sign in to \ncontinue!',
+              style: AppTextStyles.headlineMedium,
+            ),
+            const SizedBox(height: 39),
+            const FATextField(
+              hintText: 'Email Address',
+              icon: 'assets/icons/ic_tick.svg',
+            ),
+            const SizedBox(height: 14),
+            const FATextFieldPassword(
+              hintText: 'Password',
+            ),
+            const SizedBox(height: 17),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  'Forgot Password?',
+                  style: AppTextStyles.textTitle,
+                  textAlign: TextAlign.right,
+                ),
+              ],
+            ),
+            const SizedBox(height: 34),
+            FAButton(text: 'Login'),
+            const SizedBox(height: 24),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Or Login with',
+                  style: AppTextStyles.textSmall,
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            FAButton.outline(
+              icon: 'assets/icons/ic_google.svg',
+              text: 'Connect with Google',
+            ),
+            const SizedBox(height: 8),
+            FAButton.text(
+              icon: 'assets/icons/ic_facebook.svg',
+              text: 'Connect With Facebook',
+            ),
+            const SizedBox(height: 48),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: 'Don’t have an account? ',
+                        style: AppTextStyles.textAppBar.copyWith(
+                          fontWeight: AppFontWeight.medium,
+                        ),
+                      ),
+                      TextSpan(
+                          text: 'Register!', style: AppTextStyles.textAppBar),
+                    ],
                   ),
-                ],
-              ),
-              const SizedBox(height: 34),
-              FAButton(
-                text: 'Login',
-              ),
-              const SizedBox(height: 24),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Or Login with',
-                    style: AppTextStyles.textSmall,
-                  ),
-                ],
-              ),
-            ],
-          ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
