d4e0cdc056ee372b2a154b07bffed06ef120ce5a
Kieu Linh <hothikieulinh1407@gmail.com>
2023-12-18 17:26:53 +0700

    update manage theme in app

---
+++ Flutter/flutter_practice_one/apps/fitness_app/lib/core/theme/theme.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_practice_one/core/color/app_color.dart';
+import 'package:flutter_practice_one/core/theme/custom_themes/text_theme.dart';
+
+class FAppTheme {
+  FAppTheme._();
+
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Montserrat-Regular',
+    brightness: Brightness.light,
+    primaryColor: AppColor.primary,
+    scaffoldBackgroundColor: AppColor.secondary,
+    textTheme: FATextTheme.lightTextTheme,
+  );
+
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Montserrat-Regular',
+    brightness: Brightness.dark,
+    primaryColor: AppColor.primary,
+    scaffoldBackgroundColor: AppColor.tertiary,
+    textTheme: FATextTheme.darkTextTheme,
+  );
+}
